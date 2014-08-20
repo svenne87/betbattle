6067bd08db262d8d562a63b8d7819f1e6cc9557b
Lukas Andersson <lukas_andersson@live.se>
2014-08-20 16:56:56 +0200

    fa

--- app/assets/lib/FontAwesome.js
+++ app/assets/lib/FontAwesome.js
@@ -742,7 +742,7 @@ function FontAwesome(options) {
 		"fa-vimeo-square" : 0xf194,
 		"fa-try" : 0xf195,
 		"fa-plus-square-o" : 0xf196,
-		"fa-share-alt" : 0xf064,
+		"fa-share-alt" : 0xf1e0,
 	};
 
 }
